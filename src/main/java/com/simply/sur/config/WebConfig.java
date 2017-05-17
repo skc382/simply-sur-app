6a2d617fa2873779e2aa0cb7b31ceb89bcc380bc
Shreedhar KC <shreedharkempanayakanahallichampakaram@Shreedhars-MacBook-Pro.local>
2017-05-17 02:31:53 -0500

    feat: completed integration with the front end

--- src/main/java/com/simply/sur/config/WebConfig.java
+++ src/main/java/com/simply/sur/config/WebConfig.java
@@ -16,7 +16,7 @@ public class WebConfig extends WebMvcConfigurerAdapter {
         SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
         templateResolver.setPrefix("classpath:/webapp/SimplySurFrontEnd/src/app/");
         templateResolver.setSuffix(".html");
-        templateResolver.setTemplateMode("HTML5");
+        templateResolver.setTemplateMode("LEGACYHTML5");
         templateResolver.setCacheable(true);
         return templateResolver;
     }
@@ -35,9 +35,42 @@ public class WebConfig extends WebMvcConfigurerAdapter {
         return viewResolver;
     }
 
-//    @Override
-//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//        registry.addResourceHandler("/styles/**").addResourceLocations("classpath:/assets/");
-//
-//    }
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        if (!registry.hasMappingForPattern("/webapp/**")) {
+            registry.addResourceHandler("/webapp/**").addResourceLocations("classpath:/webapp/");
+        }
+
+        if (!registry.hasMappingForPattern("/resources/**")) {
+            registry.addResourceHandler("/resources/**").addResourceLocations("classpath:/webapp/SimplySurFrontEnd/src/resources/");
+        }
+
+        if (!registry.hasMappingForPattern("/about-page/**")) {
+            registry.addResourceHandler("/about-page/**").addResourceLocations("classpath:/webapp/SimplySurFrontEnd/src/app/about-page/");
+        }
+
+        if (!registry.hasMappingForPattern("/event-card-directive/**")) {
+            registry.addResourceHandler("/event-card-directive/**").addResourceLocations("classpath:/webapp/SimplySurFrontEnd/src/app/event-card-directive/");
+        }
+
+        if (!registry.hasMappingForPattern("/events-page/**")) {
+            registry.addResourceHandler("/events-page/**").addResourceLocations("classpath:/webapp/SimplySurFrontEnd/src/app/events-page/");
+        }
+
+        if (!registry.hasMappingForPattern("/gallery-page/**")) {
+            registry.addResourceHandler("/gallery-page/**").addResourceLocations("classpath:/webapp/SimplySurFrontEnd/src/app/gallery-page/");
+        }
+
+        if (!registry.hasMappingForPattern("/home-page/**")) {
+            registry.addResourceHandler("/home-page/**").addResourceLocations("classpath:/webapp/SimplySurFrontEnd/src/app/home-page/");
+        }
+
+        if (!registry.hasMappingForPattern("/services/**")) {
+            registry.addResourceHandler("/services/**").addResourceLocations("classpath:/webapp/SimplySurFrontEnd/src/app/services/");
+        }
+
+        if (!registry.hasMappingForPattern("/thumbnail-directive/**")) {
+            registry.addResourceHandler("/thumbnail-directive/**").addResourceLocations("classpath:/webapp/SimplySurFrontEnd/src/app/thumbnail-directive/");
+        }
+    }
 }
--- src/main/java/com/simply/sur/controller/AppController.java
+++ src/main/java/com/simply/sur/controller/AppController.java
@@ -11,4 +11,5 @@ public class AppController {
     public String serveIndex() {
         return "index";
     }
+
 }
