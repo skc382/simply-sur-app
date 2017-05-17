c5f39c1ed572a72eebbdeb17951ade37e4271498
Shreedhar KC <shreedharkempanayakanahallichampakaram@Shreedhars-MacBook-Pro.local>
2017-05-16 21:56:03 -0500

    feat:initial commit

---
+++ src/main/java/com/simply/sur/SimplySurApplication.java
@@ -0,0 +1,12 @@
+package com.simply.sur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplySurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplySurApplication.class, args);
+	}
+}
